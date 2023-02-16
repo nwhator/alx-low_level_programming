b19534e7c0f03f8ddb0220dc44e9cecf1f6fce72
nwhator <nwhator@gmail.com>
2023-02-16 04:36:09 -0800

    Task 4 - Prints String and Returns 0

---
+++ 0x00-hello_world/4.puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Task - Print:"Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
