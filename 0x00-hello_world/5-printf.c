d4f7188a925584480d46ba088de320701fba26c8
nwhator <nwhator@gmail.com>
2023-02-16 05:46:31 -0800

    Task 5 - Use printf to Print String and Return 0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Hello, printf
+ * Description: Using printf to print text
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
