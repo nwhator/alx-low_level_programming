e9d256fd0cca388f50234e34645a9a6b1f669904
nwhator <nwhator@gmail.com>
2023-02-16 08:40:34 -0800

    Task 8 - Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fprintf(stderr, "%s", s);
 	return (1);
