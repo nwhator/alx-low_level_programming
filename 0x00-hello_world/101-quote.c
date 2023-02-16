92c3488aedd7836a6016d77bef047091f1c80265
nwhator <nwhator@gmail.com>
2023-02-16 07:24:16 -0800

    Task 8 - Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints to string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
