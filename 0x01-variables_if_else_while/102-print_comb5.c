0ce46a789a4cf57ebce7c31fe1c8d702c0417e78
nwhator <nwhator@gmail.com>
2023-02-17 12:52:36 -0800

    Task 12 - Combination (00 - 99)

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Combination '00' - '99'
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			putchar((i / 10) + '0'); /* print first digit of i */
+			putchar((i % 10) + '0'); /* print second digit of i */
+			putchar(' ');
+			putchar((j / 10) + '0'); /* print first digit of j */
+			putchar((j % 10) + '0'); /*  print second digit of j */
+
+			if (i < 99 || j < 99) /* check if it's the last combination */
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
