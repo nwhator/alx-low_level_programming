ae6b0c9f622118cbcb00e20d42612a1d782833de
nwhator <nwhator@gmail.com>
2023-02-17 11:50:21 -0800

    Task 10 - Combination II

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Combinations of two digit
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
