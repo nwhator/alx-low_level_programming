8a2d52d1a3dd312f4572b6c6b4ad000e84999c5a
nwhator <nwhator@gmail.com>
2023-02-17 13:22:31 -0800

    Task 12 - Combination (00 - 99)

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 			putchar(' ');
 			putchar((j / 10) + '0'); /* print first digit of j */
 			putchar((j % 10) + '0'); /*  print second digit of j */
-			if (i != 99 || j != 99) /* check if it's the last combination */
+			if (i != 98 || j != 99) /* check if it's the last combination */
 			{
 				putchar(',');
 				putchar(' ');
