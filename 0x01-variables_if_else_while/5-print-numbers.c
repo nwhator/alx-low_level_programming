b9612549c3fa369e39c26ea74d5473d42e78492c
nwhator <nwhator@gmail.com>
2023-02-17 08:07:02 -0800

    Task 5 - Numbers

---
+++ 0x01-variables_if_else_while/5-print-numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Numbers, counting them right?
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
