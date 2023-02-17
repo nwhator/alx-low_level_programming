d391c6b7863b6959468019c97038ea3fe9ad487f
nwhator <nwhator@gmail.com>
2023-02-17 12:01:00 -0800

    Task 10 - Combination II

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,16 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i < 8)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar(',');
+			putchar(' ');
 		}
-		putchar('\');
 	}
-
+	putchar('\n');
 	return (0);
 }
