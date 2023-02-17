5abc35d7d13f71da966804701ceeda49fd676e7f
nwhator <nwhator@gmail.com>
2023-02-17 04:39:38 -0800

    Task 2 - The Alphabet Game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: The alphabet game
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = "a";
+
+	while (c <= "z")
+	{
+		putchar(c);
+		c++;
+	}
+	putchar("/n");
+	return (0);
+}
