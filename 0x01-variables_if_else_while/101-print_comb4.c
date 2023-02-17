dbd84e16af3df0b87fe93fdc16d3b4b4c02f92c4
nwhator <nwhator@gmail.com>
2023-02-17 12:24:29 -0800

    Tsk 11 - Combination 3 Digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Really? Combination again! Three!!!
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = a + 1; b <= 8; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a != 7 || b != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
