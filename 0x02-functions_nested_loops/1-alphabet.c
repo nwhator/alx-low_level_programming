a6e13da45d3e1440ee24e19312a65870b667315d
nwhator <nwhator@gmail.com>
2023-02-21 03:11:07 -0800

    Task 1 - Alphabet, lowercare

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,19 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Entry point
  * Description - Prints the alphabet in lowercase
- * Return: 0
+ * Return: void
  */
 
 void print_alphabet(void)
 {
+	char c = 'a';
 
+	while (c < 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
