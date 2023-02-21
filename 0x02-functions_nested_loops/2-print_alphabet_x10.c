d6795e631494f94a478753482e4e53b5a3173900
nwhator <nwhator@gmail.com>
2023-02-21 03:20:58 -0800

    Task 2 - Print Alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#inlcude "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	while (i <= 10)
+	{
+		for (c; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
