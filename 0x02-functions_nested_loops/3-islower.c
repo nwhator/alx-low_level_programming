48a302711c0c456efcbc58498fc00ea2f889d8d8
nwhator <nwhator@gmail.com>
2023-02-21 06:09:35 -0800

    Task 3 - Checks if character is Lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,14 @@
 
 /**
  * _islower - Entry point
- * Return 1 if true, 0 if false
+ * @c: The interget value
+ * Return: 1 if true, 0 if false
  */
 
 int _islower(int c)
 {
+	int  c;
+
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
