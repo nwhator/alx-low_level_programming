75935db0a24c81ce7b587a48ca5adde8f54d23fa
nwhator <nwhator@gmail.com>
2023-02-21 06:04:17 -0800

    Task 3 - Checks if character is Lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Return 1 if true, 0 if false
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
