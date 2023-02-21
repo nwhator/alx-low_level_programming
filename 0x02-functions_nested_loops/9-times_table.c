78e515c7e5293e54f16cf23a6e1a280cf23f9800
nwhator <nwhator@gmail.com>
2023-02-21 10:00:52 -0800

    Task 9 - 9 Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,15 @@
 
 void times_table(void)
 {
-	int i, j, ans;
+	int i, j, ans, tens, units;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			ans = i * j;
+			tens = ans / 10;
+			units = ans % 10;
 			if (j == 0)
 			{
 				_putchar('0');
@@ -24,14 +26,14 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(ans + '0');
+				_putchar(units + '0');
 			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(ans / 10 + '0');
-				_putchar(ans % 10 + '0');
+				_putchar(tens / 10 + '0');
+				_putchar(units % 10 + '0');
 			}
 		}
 		_putchar('\n');
