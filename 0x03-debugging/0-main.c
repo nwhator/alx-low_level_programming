12fb6191899f4385dfb8d32c59bc16c92c0c9234
nwhator <nwhator@gmail.com>
2023-02-22 08:33:05 -0800

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description - Test function that prints if integer is positive ornegative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
