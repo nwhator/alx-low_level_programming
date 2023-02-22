138ed2f6e35c6847ed94377af88c4d94109b7f9f
nwhator <nwhator@gmail.com>
2023-02-21 18:26:21 -0800

    Task 12 - n Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,14 +11,13 @@ void print_times_table(int n)
 {
 	int i, j, ans;
 
-	ans = i * j;
-
 	if (n > 15 || n < 0)
 	{
 		return;
 	}
 	for (i = 0; i <= n; i++)
 	{
+		ans = i * j;
 		for (j = 0; j <= n; j++)
 		{
 			_putchar(ans + '0');
