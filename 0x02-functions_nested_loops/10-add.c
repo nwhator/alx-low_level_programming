909e1e9eb0eb29fdd8b4a94a96d673b847dd9205
nwhator <nwhator@gmail.com>
2023-02-21 17:54:06 -0800

    Task 10 - Addition of two Integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description - Addition of two integers
+ * @i: 1st Integer
+ * @j: 2nd Integer
+ * Return: int
+ */
+
+int add(int i, int j)
+{
+	int ans = i + j;
+
+	return (ans);
+}
