d13ca12dbf6fec1d7c09a2aa0cc08ac06cd3c572
nwhator <nwhator@gmail.com>
2023-02-22 09:53:24 -0800

    Task 2 - Optimize Code

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,10 +13,16 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && b > c)
+	{
 		largest = a;
+	}
 	else if (b > a && a > c)
+	{
 		largest = b;
+	}
 	else
+	{
 		largest = c;
+	}
 	return (largest);
 }
