58abdba5b67aa82539e6fd87e52f6e9c5ea62fc1
nwhator <nwhator@gmail.com>
2023-02-21 19:21:28 -0800

    Task 12 - n Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,42 +11,42 @@ void print_times_table(int n)
 {
 	int i, j, ans;
 
-	if (n < 0 || n > 15)
+	if (n >= 0 && n <= 15)
 	{
-		return;
-	}
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			ans = i * j;
-			if (j ==  0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar('0');
+				ans = i * j;
+				if (ans > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((ans / 100) + '0');
+					_putchar(((ans / 10) % 10) + '0');
+					_putchar((ans % 10) + '0');
+				}
+				else if (ans > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((ans / 10) & 10) + '0');
+					_putchar((ans & 10) + '0');
+				}
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(ans + '0');
+				}
 			}
-			else if (ans < 10)
-			{
-				_putchar(' ');
-			}
-			else if (ans < 100)
-			{
-				_putchar(' ');
-			}
-			if (ans < 0)
-			{
-				_putchar(ans + '0');
-			}
-			else
-			{
-				_putchar(ans / 10 + '0');
-				_putchar(ans % 10 + '0');
-			}
-			if (j < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
