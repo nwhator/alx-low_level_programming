01f9d8e3c0ddf314c2d430b75cade51c4ae1caed
nwhator <nwhator@gmail.com>
2023-02-21 19:33:14 -0800

    Task 13 - Multiples 3, 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Sum of all multiples of 3 or 5 below 1024
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0l i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
