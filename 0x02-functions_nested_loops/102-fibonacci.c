2717c70c2f3df270995361a4236ffb3bec5f21d5
nwhator <nwhator@gmail.com>
2023-02-21 19:46:05 -0800

    Task 14 - Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Fibonacci Numbers (50)
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, n1, n2, n3;
+
+	n1 = 1;
+	n2 = 2;
+
+	for (i = 3; i <= 50; i++)
+	{
+		n3 = n1 + n2;
+		printf("%d", n3);
+		if (i < 50)
+		{
+			printf(", ");
+		}
+		n1 = n2;
+		n2 = n3;
+	}
+	printf("\n");
+	return (0);
+}
