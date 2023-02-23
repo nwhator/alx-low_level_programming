6a957c4fd6d3a2c94475f06d487a8e03eed2b211
nwhator <nwhator@gmail.com>
2023-02-23 02:58:34 -0800

    Task 2 - Multiples two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * Description: Multiplies two integers
+ * @a: Integer
+ * @b: Integer
+ * Return: int
+ */
+
+int mul(int a, int b)
+{
+	int ans = a * b;
+
+	return (ans);
+}
