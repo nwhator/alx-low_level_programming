5f60f2029c2b2e6c4f96cb4821446154ca9c14f7
nwhator <nwhator@gmail.com>
2023-02-23 03:17:43 -0800

    Task 3 - Prints number 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ * Description: Print 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
