ff9ed052614306c326880e322f398086fddfdb06
nwhator <nwhator@gmail.com>
2023-02-23 05:26:27 -0800

    Task 7 - Print Diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * Description: Draws a diagonal line
+ * @n: Integer variable
+ * Return: int
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
