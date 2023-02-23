652bfc59bd3c785ba5d834b4582696b406c04f21
nwhator <nwhator@gmail.com>
2023-02-23 05:41:55 -0800

    Task 8 - Print Squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ * Description: Print 8 Square
+ * @size: Integet variable
+ * Return: int;
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
