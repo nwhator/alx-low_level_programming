628cd74bbcec11735370bf6c4de9c2b6bf745e7e
nwhator <nwhator@gmail.com>
2023-02-23 02:23:47 -0800

    Task 1 - Checks for digit between 0 to 9

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -11,9 +11,10 @@ int main(void)
 {
 	char c;
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
+	c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
 	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, _isdigit(c));
 	return (0);
 }
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,4 @@
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
+
