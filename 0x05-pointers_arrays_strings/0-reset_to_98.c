1b280a1b824b3067d7160654af9ae3210ad59174
nwhator <nwhator@gmail.com>
2023-02-27 04:33:39 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * Description:  Takes a pointer to an int as parameter
+ * @n:  Integer variable
+ * Return: int
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
