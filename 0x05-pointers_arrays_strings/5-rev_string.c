0536aed4c437067c535802b1ee012dd67cea339e
nwhator <nwhator@gmail.com>
2023-02-27 11:00:45 -0800

    Task 5 - Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ * Description: Reverse string
+ * @s: Integer
+ * Return: char
+ */
+
+void rev_string(char *s)
+{
+	int len, i, j;
+	char temp;
+
+	len = 0;
+	j = len - 1;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (i < j)
+	{
+		temp =s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+}
