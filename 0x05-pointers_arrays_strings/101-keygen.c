6a8d2ebd1656b6c3c8e4da601e95cf3bfa7fa6c7
nwhator <nwhator@gmail.com>
2023-02-27 13:49:36 -0800

    Task 11 - Random Password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 11
+
+/**
+ *
+ * main - Entry point
+ * Description: generates random passwords for 101-crackme
+ * Return: void
+ */
+
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const size_t charset_length = sizeof(charset) - 1;
+	
+	srand(time(NULL));  /* Seed the random number generator with the current time */
+	
+	/* Generate a random password */
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = charset[rand() % charset_length];
+	}
+	password[PASSWORD_LENGTH] = '\0';  // Null-terminate the password
+	printf("Random password: %s\n", password);
+	return (0);
+}
