e534ca69128299edda389090729210ef88fdd1dc
nwhator <nwhator@gmail.com>
2023-03-01 11:28:47 -0800

    Task 12 - Buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <ctype.h>
 
 /**
  * print_buffer - Prints a buffer 10 bytes at a time, starting with
@@ -8,59 +7,40 @@
  *                then displaying printable charcaters.
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
- * Return: something
+ * Return: int
  */
 
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int byte, index;
 
-	if (size <= 0)
+	for (byte = 0; byte < size; byte += 10)
 	{
-		printf("\n");
-		return;
-	}
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x ", i);
-		for (j = i; j < i + 10; j += 2)
+		printf("%08x: ", byte);
+
+		for (index = 0; index < 10; index++)
 		{
-			if (j < size)
-			{
-				printf("%02x", (unsigned char)b[j]);
-			}
-			else
-			{
+			if ((index + byte) >= size)
 				printf("  ");
-			}
-			if (j + 1 < size)
-			{
-				printf("%02x ", (unsigned char)b[j + 1]);
-			}
 			else
-			{
-				printf("   ");
-			}
-		}
-		printf(" ");
-		for (j = i; j < i + 10; j++)
-		{
-			if (j < size)
-			{
-				if (isprint(b[j]))
-				{
-					printf("%c", b[j]);
-				}
-				else
-				{
-					printf(".");
-				}
-			}
-			else
-			{
+				printf("%02x", *(b + index + byte));
+			if ((index % 2) != 0 && index != 0)
 				printf(" ");
-			}
 		}
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+			else if (*(b + index + byte) >= 31 &&
+				 *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+			else
+				printf(".");
+		}
+		if (byte >= size)
+			continue;
 		printf("\n");
 	}
+	if (size <= 0)
+		printf("\n");
 }
