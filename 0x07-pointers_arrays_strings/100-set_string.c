f330e4dca4282725f8a797b1735f8ed3ae162f36
nwhator <nwhator@gmail.com>
2023-03-06 09:26:30 -0800

    Task 8: Set value to a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * Description: Sets the value of a pointer to a char
+ * @s: Character
+ * @to: Character
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
