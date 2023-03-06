46b03e7056c6f9014f0396de88e4f003a6cda357
nwhator <nwhator@gmail.com>
2023-03-06 08:50:28 -0800

    Task 7: Prints the Chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * Description: Prints the chessboard
+ * @a: Character
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
