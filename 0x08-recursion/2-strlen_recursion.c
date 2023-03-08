0e49b67ff661b4502e592c75818536963101f378
nwhator <nwhator@gmail.com>
2023-03-08 03:49:29 -0800

    Task 2: Return the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  _strlen_recursion - Entry point
+ *  Description: Returns the length of a string
+ *  @s: Character
+ *  Return: Length
+ */
+
+ int  _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 +  _strlen_recursion(s + 1));
+	}
+}		
