edee49ba475b1424bd10462cff064ceafe463f87
nwhator <nwhator@gmail.com>
2023-03-08 03:31:54 -0800

    Task 0: Prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry point
+ * Description: Prints a string, followed by a new line using recursion
+ * @s: Character
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s+1);
+}
