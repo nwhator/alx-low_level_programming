3f00184302580b1259e56a001ca415992a209522
nwhator <nwhator@gmail.com>
2023-03-08 07:19:42 -0800

    Task 5: Prints Natural root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,32 @@
 #include "main.h"
 
+int_sqrt(int n);
+
+/**
+ * _sqrt - Entry point
+ * Description: Natural square root
+ * @n: Integer
+ * Return: -1 if negative n if 0 or 1
+ */
+
+int _sqrt(int n)
+{
+	int x = _sqrt_recursion(n - 1) + 1;
+
+	if (x * x > n)
+	{
+		return (x - 1);
+	}
+	else if (x * x == n)
+	{
+		return (x);
+	}
+	else
+	{
+		return (_sqrt_recursion(n - 1) + 1);
+	}
+}
+
 /**
  * _sqrt_recursion - Entry point
  * Description: Natural square root
@@ -19,19 +46,6 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		int x = _sqrt_recursion(n - 1) + 1;
-
-		if (x * x > n)
-		{
-			return (x - 1);
-		}
-		else if (x * x == n)
-		{
-			return (x);
-		}
-		else
-		{
-			return (_sqrt_recursion(n - 1) + 1);
-		}
+		return (_sqrt(n));
 	}
 }
