352e31efb33b3a7d668d197bbc222dcf1be8af12
nwhator <nwhator@gmail.com>
2023-03-10 04:38:20 -0800

    Task 0: Static Library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Nwhator
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libmy.a.
+ */
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +27,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
