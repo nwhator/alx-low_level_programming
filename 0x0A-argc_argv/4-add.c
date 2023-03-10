db69cb7a54aaf0138609db5b9376cbb66f5798d2
nwhator <nwhator@gmail.com>
2023-03-10 08:33:28 -0800

    Task 4: Add +ve Numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,7 @@
 int main(int argc, char *argv[])
 {
 	int i, j, sum;
-	
+
 	sum = 0;
 
 	if (argc == 1)
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	for (i = 1; i< argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
