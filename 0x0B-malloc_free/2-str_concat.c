a3f82f77653f7f593ccd68cbc2a607cb37f0b021
nwhator <nwhator@gmail.com>
2023-03-13 11:17:11 -0700

    Task 2: Concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = 0, len2 = 0, i;
+	int len = 0, i, j = 0;
 	char *ans;
 
 	if (s1 == NULL)
@@ -20,33 +20,26 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s2 == NULL)
 	{
-		s1 = "";
-	} /* Treat string as empty if its NULL */
-	while (s1[len1] != '\0')
-	{
-		len1++;
+		s2 = "";
 	}
-	while (s2[len2] != '\0')
+	for (i = 0; s1[i] || s2[i]; i++)
 	{
-		len2++;
-	} /* Gets the length of each string */
+		len++;
+	}
 
-	ans = malloc((len1 + len2 + 1) * sizeof(char));
+	ans = malloc((len) * sizeof(char));
 	/* Allocates memory for the ans string */
 	if (ans == NULL)
 	{
 		return (NULL); /* Allocation failed */
 	}
-	for (i = 0; i < len1; i++)
+	for (i = 0; s1[i]; i++)
 	{
-		ans[i] = s1[i];
+		ans[j++] = s1[i];
 	} /* Copies contents of s1 to the ans string */
-	for (i = 0; i < len2; i++)
+	for (i = 0; s2[i]; i++)
 	{
-		ans[len1 + i] = s2[i];
+		ans[j++] = s2[i];
 	} /* Copies contents of s2 to the ans string */
-	ans[len1 + len2] = '\0';
-	/* Terminates ans string with null */
 	return (ans);
-	free(ans);
 }
