ca3deaee3330e3ee71cdcd2b6048720646df8d57
nwhator <nwhator@gmail.com>
2023-03-20 15:16:53 -0700

    Task 0: Define struct dog with name, age, and owner fields

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Entry point
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: Define struct dog with the three members
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
