c11146a1fa4834b397afa182ca5f470bcfe526b4
nwhator <nwhator@gmail.com>
2023-03-20 06:31:47 -0700

    Task 2: Add C program to print name of compiled file using FILE macro.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: Prints the name of the file it was compiled from,
+ * followed by a new line
+ * Return: 0
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
