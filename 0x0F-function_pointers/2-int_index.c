7766df2b34932b4f002b88b31667f5f4c2c6d5c0
nwhator <nwhator@gmail.com>
2023-03-22 12:02:53 -0700

    Implement int_index function in C

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,23 +10,23 @@
  * Return: -1 if no element matches If size <= 0, return -1
  */
 
- int int_index(int *array, int size, int (*cmp)(int))
- {
-	 int i, result;
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, result;
 
-	 if (!array || !cmp || size <= 0)
-	 {
-		 return (-1);
-		 /* if input invalid */
-	 }
-	 for (i = 0; i < size; i++)
-	 {
-		 result = cmp(array[i]);
-		 if (result != 0)
-		 {
-			 return (i);
-			 /* index of matching element */
-		 }
-	 }
-	 return (-1); /* no matching element found */
- }
+	if (!array || !cmp || size <= 0)
+	{
+		return (-1);
+		/* if input invalid */
+	}
+	for (i = 0; i < size; i++)
+	{
+		result = cmp(array[i]);
+		if (result != 0)
+		{
+			return (i);
+			/* index of matching element */
+		}
+	}
+	return (-1); /* no matching element found */
+}
