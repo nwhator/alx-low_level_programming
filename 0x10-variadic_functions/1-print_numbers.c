19be34e24b7447accf3d22f65a7189a6ddfa5784
nwhator <nwhator@gmail.com>
2023-03-23 16:17:11 -0700

    Function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	va_start(args, n);	
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
