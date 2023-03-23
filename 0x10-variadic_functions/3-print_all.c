4a04b5d3c0b2cdd9bddfa9a3b97a42f85c63c460
nwhator <nwhator@gmail.com>
2023-03-23 16:35:35 -0700

    Add feature to print anything using variable arguments

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+
+/**
+ * print_all - function that prints anything.
+ *
+ * @format: a list of types of arguments passed to the function.
+ * Return: void.
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *s;
+
+	va_start(args, format);
+	/* loop through the format string */
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", (float) va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+			default:
+				break;
+		}
+		/* if there is another format specifier */
+		if (format[i + 1])
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
