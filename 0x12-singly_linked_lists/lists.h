c2cf4c8c4604423e6a07b7ff76a2f8fcd7046014
nwhator <nwhator@gmail.com>
2023-03-30 04:48:58 -0700

    Prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*
+* Desc: header file containing prototypes in current project
+*/
+
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,15 +16,15 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *              for Holberton project
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
