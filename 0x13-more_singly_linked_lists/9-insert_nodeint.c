68eb1f8558eee5248f1bb3b1b220a5c066b87669
nwhator <nwhator@gmail.com>
2023-04-04 02:09:48 -0700

    Inserts a new node at a given position.

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Entry point
+ * Description: Inserts a new node at a given position.
+ * @head: Pointer to pointer of the first node in the linked list
+ * @idx: Index of the list where the new node should be added.
+ * @n: The new node
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *current;
+	/* Where new is the new node, current as it is, i is a node */
+	unsigned int i;
+
+	/* Checks if head is empty */
+	if (!head)
+	{
+		return (NULL);
+	}
+	/* Allocates memory to new node */
+	new = malloc(sizeof(listint_t));
+	/* Checks if new node is empty and sets its data */
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	/* Insert idx at beginning of the list(starts at 0) and adds new node */
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	/* Locate node before location for new node, then insert new node  */
+	current = *head;
+	for (i = 0; i < (idx - 1); i++)
+	{
+		current  = current->next;
+	}
+	/* Insert new node between current and new node */
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
+}
