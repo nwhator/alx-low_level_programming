0530c241748d7543d6738b7da4f8e934d44982a4
nwhator <nwhator@gmail.com>
2023-04-07 18:06:04 -0700

    Reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/*
+ * read_textfile - Entry point
+ * Description: Reads a text file and prints it to the POSIX standard output
+ * @filename: Name of the file to be read
+ * @letters: The number of letters it should read and print
+ *
+ * Return: The actual number of letters it could read and print
+ *		if the file can not be opened or read, return 0
+ *		if filename is NULL return 0
+ *		if write fails or does not write the expected amount of bytes, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	/* File descriptor */
+	int fd;
+	/* Buffer to store text */
+	char *buffer;
+	/* Number of bytes read or written */
+	ssize_t my_read, my_write;
+
+	/* Check if filename is NULL */
+	if (!filename)
+	{
+		return (0);
+	}
+	/* Open the file to be read and store fd*/
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	/* Allocate memory for the buffer */
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(fd);
+		return (0);
+	}
+	/* Read from the file and stores number of bytes read */
+	my_read = read(fd, buffer, letters);
+	if (my_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	/* Write to standard output and store the number of bytes written */
+	my_write = write(STDOUT_FILENO, buffer, my_read);
+	if (my_write != my_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	/* Free the memory and close the file descriptor (fd) */
+	free(buffer);
+	close(fd);
+
+	return (my_write);
+}
