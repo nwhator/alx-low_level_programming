932f44375f859a47f4fa77fb5091b2290b49cdea
nwhator <nwhator@gmail.com>
2023-04-07 17:24:16 -0700

    Header file created main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,20 @@
+#ifndef MY_MAIN_H
+#define MY_MAIN_H
+
+/*
+ * Description: Header file containing prototypes for all functions
+ * for the 0x15-file_io project
+ */
+
+/* Libraries Used in the Project */
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/* Prototypes */
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MY_MAIN_H */
